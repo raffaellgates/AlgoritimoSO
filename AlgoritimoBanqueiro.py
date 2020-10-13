e31bb3dfcb9cde4f67147bb01fcdae8d15a176f4
rafaGates <rafael12silva73@gmail.com>
2020-10-13 02:12:57 -0300

    dicas pra dar certo

--- AlgoritimoBanqueiro.py
+++ AlgoritimoBanqueiro.py
@@ -1,24 +1,30 @@
 def atual_alocacao(processo):
+    #retonar uma lista com a alocação corrente do processo ex "return processo = [1, 0, 1, 0]"
     pass
 
 
 def requisito_maximo(processo):
+    #retonar uma lista com Recursos existentes fazendo a o calculo c + a = e
     pass
 
 
 def subtrair_atualAlocação_requisitoMaximo(atual_alocação, requisito_maximo):
+    #retornar uma lista do  resultado da subtracao das listas "requisito_maximo - atual_alocação" 
     pass
 
 
 def eh_possivel_realizar_processo(subt_mp_cp, recursos_livres):
+    #retonar true ou false se $1 > $2 comparando a listas
     pass
 
 
 def atualiza(recursos_livres, atual_alocação):
+    #retonar uma lista somando a duas lista
     pass
 
 
 def remove_elemento_do_conjunto(processo, conjunto_de_processos):
+    #remover processo do conjunto
     pass
 
 
@@ -34,7 +40,7 @@ def algoritmo_do_banqueiro(conjunto_de_processos, recursos_livres):
                 # p pode obter tudo de que necessita.
                 # Suponha que ele faz isso, termina, e libera o que ele já tem.
                 recursos_livres = atualiza(recursos_livres, atual_alocação)
-                remove_elemento_do_conjunto(processo, conjunto_de_processos)
+                conjunto_de_processos = remove_elemento_do_conjunto(processo, conjunto_de_processos)
                 deadlock = True
 
         if (not deadlock):
